d18d08ef0a56548add1bd48e70d5c3a9f5dd8562
sponberry <74988392+sponberry@users.noreply.github.com>
2021-04-13 21:09:27 +0100

    uninstalled bootstrap, install react-bootstrap, added some css and button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello world
+      <button className="btn btn-light enter-button">Gallery</button>
     </div>
   );
 }
