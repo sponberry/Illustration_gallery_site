b22021c349fbd15385a6a184dd182c3459d47771
abigailNottingham <abigailillustration@gmail.com>
2021-05-22 10:26:27 +0100

    fixed navbar spacing on smaller devices.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Navbar, Nav } from "react-bootstrap"
 
+import "../styles/Navbar.css"
+
 const NavBar = () => (
   <Navbar collapseOnSelect expand="false" bg="none" fixed="top">
     <Navbar.Brand href="#home" className="page-title">abigailillustration</Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" className="dropdown-btn"/>
     <Navbar.Collapse id="responsive-navbar-nav">
       <Nav className="text-lg-right text-center">
         <Nav.Link href="#gallery" className="menu-links">Gallery</Nav.Link>
