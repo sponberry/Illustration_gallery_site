a10963331601bdc4b7150fec074a2a6b34b30896
abigailNottingham <abigailillustration@gmail.com>
2021-07-12 07:33:23 +0100

    added stock links for images on carousel

--- src/components/ReactCarousel.js
+++ src/components/ReactCarousel.js
@@ -16,7 +16,7 @@ const _items = [
             url: require("../img/yogic_squat 2 IG copy.jpg"),
             altText: "A yogi smiles softly in a yogic squat postion",
             redBubble:"https://www.redbubble.com/i/greeting-card/Ground-Into-Peace-by-sponberry/79480581.5MT14",
-            stock: ""
+            stock: "https://www.shutterstock.com/image-illustration/peaceful-yoga-asana-pose-woman-practicing-1984706156"
           },
     },
     {
@@ -24,7 +24,7 @@ const _items = [
             url: require("../img/yoga_tree_pose.jpg"),
             altText: "A yogi balances in tree pose on short chunky legs, she is backed by leaf-shaped wings",
             redBubble: "https://www.redbubble.com/i/ipad-case/Yoga-Tree-Joy-by-sponberry/77972065.MNKGF",
-            stock: ""
+            stock: "https://www.shutterstock.com/image-illustration/happy-peaceful-yoga-tree-pose-cheerful-1984706159"
           },
     },
     {
@@ -32,7 +32,7 @@ const _items = [
             url: require("../img/yoga_mountain_1.jpg"),
             altText: "A yogi wearing a flower-crown raises their arms joyfully to the sky in a variation of mountain pose",
             redBubble: "https://www.redbubble.com/i/photographic-print/Yoga-Mountain-Pose-by-sponberry/79480320.6Q0TX",
-            stock: ""
+            stock: "https://www.shutterstock.com/image-illustration/woman-yoga-pose-mountain-volcano-asana-1984697447"
           },
     },
     {
---
+++ src/img/imagePaths.js
@@ -0,0 +1,6 @@
+export const imagePaths = {
+  "yogicSquat": require("../img/yogic_squat 2 IG copy.jpg"),
+  "treePose": require("../img/yoga_tree_pose.jpg"),
+  "mountainPose": require("../img/yoga_mountain_1.jpg"),
+  "pyramidPose": require("../img/Pyramid_Pose.jpg"),
+}
