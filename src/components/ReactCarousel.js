14a0c09fefefa87e35542882303c7773f0460123
abigailNottingham <abigailillustration@gmail.com>
2021-07-12 07:35:17 +0100

    separated object of image items into its own file to be imported to carousel component.

--- src/components/ReactCarousel.js
+++ src/components/ReactCarousel.js
@@ -1,6 +1,7 @@
 import React from "react"
 import CarouselSlideItem from "./CarouselSlideItem"
 import "../styles/carousel.scss"
+import { _items } from "./items";
 
 /************************************
 1. If you want to add or remove items you will need to change a variable called $slide-count in the CSS *minimum 3 slides
@@ -8,42 +9,6 @@ import "../styles/carousel.scss"
 2. if you want to change the dimensions of the slides you will need to edit the slideWidth variable here 👇 and the $slide-width variable in the CSS.
 ************************************/
 const slideWidth = 30;
-
-// add reference to itemServices to download items from db
-const _items = [
-    {
-        image: {
-            url: require("../img/yogic_squat 2 IG copy.jpg"),
-            altText: "A yogi smiles softly in a yogic squat postion",
-            redBubble:"https://www.redbubble.com/i/greeting-card/Ground-Into-Peace-by-sponberry/79480581.5MT14",
-            stock: "https://www.shutterstock.com/image-illustration/peaceful-yoga-asana-pose-woman-practicing-1984706156"
-          },
-    },
-    {
-        image: {
-            url: require("../img/yoga_tree_pose.jpg"),
-            altText: "A yogi balances in tree pose on short chunky legs, she is backed by leaf-shaped wings",
-            redBubble: "https://www.redbubble.com/i/ipad-case/Yoga-Tree-Joy-by-sponberry/77972065.MNKGF",
-            stock: "https://www.shutterstock.com/image-illustration/happy-peaceful-yoga-tree-pose-cheerful-1984706159"
-          },
-    },
-    {
-        image: {
-            url: require("../img/yoga_mountain_1.jpg"),
-            altText: "A yogi wearing a flower-crown raises their arms joyfully to the sky in a variation of mountain pose",
-            redBubble: "https://www.redbubble.com/i/photographic-print/Yoga-Mountain-Pose-by-sponberry/79480320.6Q0TX",
-            stock: "https://www.shutterstock.com/image-illustration/woman-yoga-pose-mountain-volcano-asana-1984697447"
-          },
-    },
-    {
-        image: {
-            url: require("../img/Pyramid_Pose.jpg"),
-            altText: "A yogi reaches down in pyramid pose, her legs illustrated in the angular style of a pyramid",
-            redBubble: "https://www.redbubble.com/i/art-print/Yoga-Calm-Pyramid-Pose-by-sponberry/79474718.1G4ZT",
-            stock: "https://www.shutterstock.com/image-illustration/woman-pyramid-yoga-pose-drawing-doing-1743200954"
-          },
-    },
-];
 
 const length = _items.length;
 _items.push(..._items);
